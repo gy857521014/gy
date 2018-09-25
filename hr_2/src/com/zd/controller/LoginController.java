4d4c25652c179c0db90fbb78557c8215bb808f52
gaoyan666 <gaoyan666@DESKTOP-R98657R>
2018-09-25 11:01:12 +0800

    框架

---
+++ hr_2/src/com/zd/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.zd.controller;
+
+public class LoginController {
+
+}
---
+++ hr_2/src/com/zd/service/impl/LoginService.java
@@ -0,0 +1,10 @@
+package com.zd.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.zd.service.ILoginService;
+
+@Service
+public class LoginService implements ILoginService{
+
+}
