69d0bcd79e7f5267c7de09d1d760ea014ddc38ca
Administrator <Administrator@PC-20171213XNFU>
2018-09-29 17:16:26 +0800

    、、

--- hr_2/src/com/zd/entity/Config_major_kind.java
+++ hr_2/src/com/zd/entity/Config_major_kind.java
@@ -2,7 +2,7 @@ package com.zd.entity;
 
 public class Config_major_kind {
 	private int mfk_id;	//主键，自动增长列 
-	private int major_kind_id;	//职位分类编号
+	private String major_kind_id;	//职位分类编号
 	private String major_kind_name;//	职分类位名称
 	public int getMfk_id() {
 		return mfk_id;
@@ -10,10 +10,11 @@ public class Config_major_kind {
 	public void setMfk_id(int mfk_id) {
 		this.mfk_id = mfk_id;
 	}
-	public int getMajor_kind_id() {
+	
+	public String getMajor_kind_id() {
 		return major_kind_id;
 	}
-	public void setMajor_kind_id(int major_kind_id) {
+	public void setMajor_kind_id(String major_kind_id) {
 		this.major_kind_id = major_kind_id;
 	}
 	public String getMajor_kind_name() {
@@ -22,7 +23,7 @@ public class Config_major_kind {
 	public void setMajor_kind_name(String major_kind_name) {
 		this.major_kind_name = major_kind_name;
 	}
-	public Config_major_kind(int mfk_id, int major_kind_id, String major_kind_name) {
+	public Config_major_kind(int mfk_id, String major_kind_id, String major_kind_name) {
 		super();
 		this.mfk_id = mfk_id;
 		this.major_kind_id = major_kind_id;
