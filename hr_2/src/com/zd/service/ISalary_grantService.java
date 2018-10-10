69fb0e8f44b0626537766fbfef2a5c3d541c4ddb
M-MinGQianG <M-MinGQianG@LAPTOP-RCFO5LP9>
2018-10-10 09:10:12 +0800

    //

--- hr_2/src/com/zd/service/ISalary_grantService.java
+++ hr_2/src/com/zd/service/ISalary_grantService.java
@@ -3,6 +3,11 @@ package com.zd.service;
 import java.util.List;
 import java.util.Map;
 
+import com.zd.entity.Config_public_char;
+import com.zd.entity.Humman_file;
+import com.zd.entity.Salary_grant_details;
+import com.zd.entity.Salary_standard_details;
+
 public interface ISalary_grantService {
 
 	public int selnumzz();
@@ -22,4 +27,17 @@ public interface ISalary_grantService {
 	public int shifaaa();
 	
 	public int zonggg();
+	
+	public List<Config_public_char> selpubulic();
+	
+	public List<Humman_file> selhuman(String first_king_name);
+	
+	public int selsum(String first_king_name);
+	
+	public int selhum(String first_king_name);
+	
+	public List<Salary_standard_details> selBySSD(String ssd);
+	
+	public void add(Salary_grant_details sgd);
+	
 }
